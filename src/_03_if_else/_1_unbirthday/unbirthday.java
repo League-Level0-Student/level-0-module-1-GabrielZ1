1c576d8063bae4fddd307e709816bf1c7db97903
GabrielZ1 <GabrielZ1@localhost>
2023-03-12 20:30:32 -0700

    unbirthday

---
+++ src/_03_if_else/_1_unbirthday/unbirthday.java
@@ -0,0 +1,35 @@
+package _03_if_else._1_unbirthday;
+//    Copyright (c) The League of Amazing Programmers 2013-2019
+//    Level 0
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+
+//Ask the user their birthday
+	
+String birthday = JOptionPane.showInputDialog("When is your birthday (mm/dd)?");
+
+
+//If it matches today's date, wish them a happy birthday
+
+if (birthday.equals("03/12")) {
+
+JOptionPane.showMessageDialog(null, "Happy Birthday!!!");
+	
+}
+
+//Otherwise, wish them a very merry UNbirthday
+
+else {
+JOptionPane.showMessageDialog(null, "Then Happy Unbirthday!");
+	
+}
+		
+	}
+}
+
